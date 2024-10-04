b4cac59ea6761c796c31dd70718558168c9e2607
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-10-04 20:32:05 +0700

    selection exp2

--- Selection2Exp2244107060048.java
+++ Selection2Exp2244107060048.java
@@ -16,8 +16,12 @@ public class Selection2Exp2244107060048 {
         if(totalAngle == 180){
             if(angle1 == 90 || angle2 == 90 || angle3 == 90){
                 System.out.println("Right Triangle");
+            }else if(angle1 == angle2 && angle2 == angle3){
+                System.out.println("Equilateral Triangle");
+            }else if(angle1 == angle2 || angle2 == angle3 || angle3 == angle1){
+                System.out.println("Isosceles Triangle");
             }else{
-                System.out.println("Not a Right Triangle");
+                System.out.println("scalene");
             }
         }else{
             System.out.println("Not a Triangle");
